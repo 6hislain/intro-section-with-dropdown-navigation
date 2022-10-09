e08f789bfb2581a9e397941fcaf37cc0653b323d
6hislain <ghislainmike80@gmail.com>
2022-10-09 13:57:28 +0200

    almost done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let showMenu = true;
-  document.querySelector(".menu-button").addEventListener("click", function () {
-    document.getElementsByClassName("navbar")[0].style.flexDirection = showMenu
-      ? "column"
-      : "row";
-    document.getElementsByClassName("menu-flex")[0].style.flexDirection =
-      showMenu ? "column" : "row";
-    document.querySelectorAll(".nav-item").forEach((e, i) => {
-      if (!showMenu) e.style.display = "none";
-      else {
-        e.style.display = "inherit";
-        e.style.width = "100%";
-      }
-    });
-    showMenu = !showMenu;
-  });
-});
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
